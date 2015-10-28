e095794bda3a3f7646a541e0d2136a4126e60c3e
way0750 <bruce0750@gmail.com>
2015-10-27 17:05:39 -0700

    Complete refactor to express

--- server/basic-server.js
+++ server/basic-server.js
@@ -1,41 +1,67 @@
 /* Import node's http module: */
-var http = require("http");
-var handleRequest = require("./request-handler.js").requestHandler
+var dataBase = {results: []};
+var fs = require('fs');
+var urlParser = require("url");
 
-// Every server needs to listen on a port with a unique number. The
-// standard port for HTTP servers is port 80, but that port is
-// normally already claimed by another server and/or not accessible
-// so we'll use a standard testing port like 3000, other common development
-// ports are 8080 and 1337.
+var express = require('express');
+var bodyParser = require('body-parser');
+var objectId = 2;
+var getJSON = bodyParser.json();
+var app = express();
+
+var headers = {
+  "access-control-allow-origin": "*",
+  "access-control-allow-methods": "GET, POST, PUT, DELETE, OPTIONS",
+  "access-control-allow-headers": "content-type, accept",
+  "access-control-max-age": 10, // Seconds.
+  'Content-Type': "application/json"
+};
+
+app.use(getJSON);
+
+// all request with the root route 'clinet' will be 200 ok and simpliy be served;
+app.use(express.static('client'))
+
+var dataBase;
+
+// fs.readFile('./server/db.txt', 'utf-8', function (err, data) {
+// 	dataBase = JSON.parse(data);
+// })
+
+var toSendMessages = function (req, res, next) {
+	res.set(headers);
+	res.send(dataBase);
+};
+
+var toWriteMessages = function (req, res, next) {
+	var newMessge = req.body;
+	console.log(newMessge);
+	newMessge.objectId = objectId++;
+	newMessge.createdAt = new Date();
+
+	// send new message to dataBase;
+	dataBase.results.push(newMessge);
+
+	//send a copy to the file;
+	fs.writeFile('./server/db.txt', JSON.stringify(dataBase), function () {})
+
+	res.set(headers);
+	res.status(201).send(dataBase);	
+};
+
+app.route('/classes/messages')
+   .get(toSendMessages)
+   .post(toWriteMessages);
+
+app.route('/classes/room1/')
+   .get(toSendMessages)
+   .post(toWriteMessages);
+
+app.use('/', function (req, res, next) {
+	res.sendStatus(404);
+});
+
 var port = 3000;
 
-// For now, since you're running this server on your local machine,
-// we'll have it listen on the IP address 127.0.0.1, which is a
-// special address that always refers to localhost.
-var ip = "127.0.0.1";
-
-
-
-// We use node's http module to create a server.
-//
-// The function we pass to http.createServer will be used to handle all
-// incoming requests.
-//
-// After creating the server, we will tell it to listen on the given port and IP. */
-var server = http.createServer(handleRequest);
-console.log("Listening on http://" + ip + ":" + port);
-server.listen(port, ip);
-
-// To start this server, run:
-//
-//   node basic-server.js
-//
-// on the command line.
-//
-// To connect to the server, load http://127.0.0.1:3000 in your web
-// browser.
-//
-// server.listen() will continue running as long as there is the
-// possibility of serving more requests. To stop your server, hit
-// Ctrl-C on the command line.
-
+app.listen(port);
+console.log('server running;')
--- server/request-handler.js
+++ server/request-handler.js
@@ -5,122 +5,59 @@ var dataBase = {results: [{username : 'god', createdAt : new Date(), objectId : 
 var fs = require('fs')
 var toFile = JSON.stringify(dataBase)
 
-fs.writeFile('./db.txt', toFile, function () {})
 
 fs.readFile('./db.txt', 'utf8', function (err, data) {
 
 })
 
 
-var defaultCorsHeaders = {
+var headers = {
   "access-control-allow-origin": "*",
   "access-control-allow-methods": "GET, POST, PUT, DELETE, OPTIONS",
   "access-control-allow-headers": "content-type, accept",
-  "access-control-max-age": 10 // Seconds.
+  "access-control-max-age": 10, // Seconds.
+  'Content-Type': "application/json"
 };
+var objectId = 1;
+var sendResponse = function(response, message, statusCode){
+  response.writeHead(statusCode, headers);
+  response.end(JSON.stringify(message));
+}
 
-var headers = defaultCorsHeaders; 
-var statusCode;
-/*************************************************************
+var receivePost = function(request, response){
+  var data = ""
+  request.on('data', function(chunk){
+    data += chunk;
+  });
+  request.on('end', function(data){
+    data = JSON.parse(data);
+    data.objectId = objectId++;
+    data.createdAt = new Date();
+    dataBase.results.push(data);
+    fs.writeFile('./db.txt', JSON.stringify(dataBase), function () {})
+  })
+  sendResponse(response, null, 201)
+}
+var actions = {
+  'GET' : function(request, response){
+    sendResponse(response, dataBase, 200)
+  },
+  'POST' : function(request, response){
+    receivePost(request, response)
+  },
+  'OPTIONS' : function(request, response){
+    sendResponse(response, 'You got options', 200);
+  }
+}
 
-You should implement your request handler function in this file.
-
-requestHandler is already getting passed to http.createServer()
-in basic-server.js, but it won't work as is.
-
-You'll have to figure out a way to export this function from
-this file and include it in basic-server.js so that it actually works.
-
-*Hint* Check out the node module documentation at http://nodejs.org/api/modules.html.
-
-**************************************************************/
 
 module.exports.requestHandler = function(request, response) {
 
   console.log("Serving request type " + request.method + " for url " + request.url);
-  var route = request.url.split('/');
-  // if route[1] === classes
-    //set up rooms
-  // if ro
-
-  /**************************POSTS*********************/
-
-  if(request.method === 'POST' && /classes/.test(request.url)){
-    statusCode = 201;
-    request.on('data', function(data){
-      data = JSON.parse(data);
-      data.objectId = Math.random();
-      data.createdAt = new Date();
-      dataBase.results.push(data);
-    });
-    fs.writeFile('./db.txt', JSON.stringify(dataBase), function () {})
-    headers['Content-Type'] = "application/json";
-    request.on('end', function(){
-      response.writeHead(statusCode, headers);
-      response.end(JSON.stringify(dataBase));
-    });
-  } else if (request.method === 'POST' && /classes/.test(request.url)){
-    statusCode = 201;
-    request.on('data', function(data){
-      data = JSON.parse(data);
-      data.objectId = Math.random();
-      data.createdAt = new Date();
-      dataBase.results.push(data);
-    });
-    headers['Content-Type'] = "application/json";
-    request.on('end', function(){
-      response.writeHead(statusCode, headers);
-      response.end(JSON.stringify(dataBase));
-    });
-  } else if (request.method === 'GET' && /classes/.test(request.url)) {
-    headers['Content-Type'] = "application/json";
-    response.writeHead(200, headers);
-    fs.readFile('./db.txt', 'utf8', function (err, data) {
-      response.end(data)
-    });
-    // response.end(JSON.stringify(dataBase));
-  }else if (request.method === 'GET' && /classes/.test(request.url)) {
-    headers['Content-Type'] = "application/json";
-    response.writeHead(200, headers);
-    response.end(JSON.stringify(dataBase));
-  } else if (request.method === "OPTIONS"){
-    response.writeHead(200, headers);
-    response.end();
+  var action = actions[request.method];
+  if( action ){
+    action(request, response)
+  } else {
+    console.log('You screwed up')
   }
-  else {
-    headers['Content-Type'] = "application/json";
-    response.writeHead(404, headers);
-    response.end();
-  }
-
-  /*************************GET**************************/  
-  
-  //Not found response
-  // else if (!/classes|send/.test(request.url) ) {
-  //   statusCode = 404;
-  //   headers['Content-Type'] = "application/json";
-  //   response.writeHead(statusCode, headers);
-  //   response.end();
-  
-  // // Successful GET response.
-  // } else {
-   
-  //   statusCode = 200;
-
-  //   headers['Content-Type'] = "application/json";
-  //   response.writeHead(statusCode, headers);
-  //   response.write(JSON.stringify(dataBase));
-  //   response.end()
-  // }
 };
-
-// These headers will allow Cross-Origin Resource Sharing (CORS).
-// This code allows this server to talk to websites that
-// are on different domains, for instance, your chat client.
-//
-// Your chat client is running from a url like file://your/chat/client/index.html,
-// which is considered a different domain.
-//
-// Another way to get around this restriction is to serve you chat
-// client from this domain by setting up static file serving.
-
